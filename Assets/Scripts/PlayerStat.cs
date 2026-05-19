4796e367e11bab150d8c8c6fb1c5952e19521ffc
votanthien <votanthien511@gmail.com>
2026-05-20 00:10:39 +0700

    upui

--- Assets/Scripts/PlayerStat.cs
+++ Assets/Scripts/PlayerStat.cs
@@ -1,13 +1,97 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerStat : MonoBehaviour
 {
     [Header("RPG Stats")]
     public int playerMaxHp = 100;
     public int playerCurrentHp = 100;
+
     public int playerMana = 0;
     public int playerMaxMana = 50;
+
+    [Header("Shield")]
     public int playerShield = 0;
+    public int maxShield = 2;
+
     public int playerAttack = 10;
-    public int playerSp = 15; // Spell Power
+    public int playerSp = 15;
+
+    [Header("UI")]
+    public Slider hpSlider;
+    public Slider manaSlider;
+
+    public TextMeshProUGUI shieldText;
+
+    // Image của icon khiên
+    public Image shieldImage;
+    public GameObject khien1;
+    public GameObject khien2;
+
+    // Màu
+    Color emptyShieldColor;
+    Color activeShieldColor;
+
+    private void Start()
+    {
+        ColorUtility.TryParseHtmlString("#202020", out emptyShieldColor);
+        ColorUtility.TryParseHtmlString("#FFFFFF", out activeShieldColor);
+        khien1.SetActive(false);
+        khien2.SetActive(false);
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        // HP
+        hpSlider.maxValue = playerMaxHp;
+        hpSlider.value = playerCurrentHp;
+
+        // Mana
+        manaSlider.maxValue = playerMaxMana;
+        manaSlider.value = playerMana;
+
+        // Text số khiên
+        shieldText.text = playerShield + "/2";
+
+        // Đổi màu ICON khiên
+        if (playerShield <= 0)
+        {
+            shieldImage.color = emptyShieldColor;
+            khien1.SetActive(false);
+            khien2.SetActive(false);
+        }
+        if (playerShield == 1)
+        {
+            shieldImage.color = activeShieldColor; 
+            khien1.SetActive(true);
+            khien2.SetActive(false);
+        }
+        if (playerShield == 2)
+        {
+            shieldImage.color = activeShieldColor; 
+            khien1.SetActive(true);
+            khien2.SetActive(true);
+        }
+       
+    }
+
+    public void AddShield(int amount)
+    {
+        if (playerShield >= maxShield)
+            return;
+
+        playerShield += amount;
+
+        if (playerShield > maxShield)
+            playerShield = maxShield;
+
+        UpdateUI();
+    }
 }
--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemy : MonoBehaviour
 {
     public int enemyMaxHp = 200;
     public int enemyCurrentHp = 200;
-   public int enemyAttack=15;
+    public int enemyAttack = 15;
+
+    [Header("UI")]
+    public Slider hpSlider;
+
+    private void Update()
+    {
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        hpSlider.maxValue = enemyMaxHp;
+        hpSlider.value = enemyCurrentHp;
+    }
 }
