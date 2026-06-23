6bdb8b749b6907164f3b05693b67c8d9b0010f1f
Phuoc1810 <phuoc1122181@gmail.com>
2026-06-23 21:21:17 +0700

    menu

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,11 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
